d70e4081db4457ed94fbd935b319c300984b1574
lsh <rrgks@naver.com>
2022-07-23 20:43:08 +0900

    feat: metric range 적용

--- metric/node/customNodeMemoryUsage.js
+++ metric/node/customNodeMemoryUsage.js
@@ -1,24 +1,33 @@
 const promClient = require('prom-client');
 
 const getMetricConfig = require('../common/getMetricConfig');
-const { INTERVAL_TIME, METRIC_NAME } = require('../constants/common');
+const getRandomValue = require('../common/randomValue');
+const {
+  INTERVAL_TIME,
+  METRIC_NAME,
+  INSTANCE_TYPE,
+  RESOURCE_NAME,
+} = require('../constants/common');
 const { NODE_LABEL_NAMES } = require('../constants/labelName');
 const { nodes } = require('../constants/nodes');
 
-const customNodeTotalMemory = (register) => {
+const customNodeMemoryUsage = (register) => {
   const metricConfig = getMetricConfig(
-    METRIC_NAME.CUSTOM_NODE_TOTAL_MEMORY,
+    METRIC_NAME.CUSTOM_NODE_MEMORY_USAGE,
     NODE_LABEL_NAMES
   );
   const g = new promClient.Gauge(metricConfig);
 
   setInterval(() => {
     nodes.forEach((node) => {
-      g.set(node, Math.random());
+      g.set(
+        node,
+        getRandomValue(INSTANCE_TYPE.NODE, RESOURCE_NAME.MEMORY_USAGE)
+      );
     });
   }, INTERVAL_TIME);
 
   register.registerMetric(g);
 };
 
-module.exports = { customNodeTotalMemory };
+module.exports = { customNodeMemoryUsage };
--- metric/pod/customPodMemoryUsage.js
+++ metric/pod/customPodMemoryUsage.js
@@ -1,21 +1,28 @@
 const promClient = require('prom-client');
 
 const getMetricConfig = require('../common/getMetricConfig');
+const getRandomValue = require('../common/randomValue');
+const {
+  INTERVAL_TIME,
+  METRIC_NAME,
+  INSTANCE_TYPE,
+  RESOURCE_NAME,
+} = require('../constants/common');
 const { POD_LABEL_NAMES } = require('../constants/labelName');
 const { pods } = require('../constants/pods');
 
 const customPodMemoryUsage = (register) => {
   const metricConfig = getMetricConfig(
-    'custom_pod_memory_usage',
+    METRIC_NAME.CUSTOM_POD_MEMORY_USAGE,
     POD_LABEL_NAMES
   );
   const g = new promClient.Gauge(metricConfig);
 
   setInterval(() => {
     pods.forEach((pod) => {
-      g.set(pod, Math.random());
+      g.set(pod, getRandomValue(INSTANCE_TYPE.POD, RESOURCE_NAME.MEMORY_USAGE));
     });
-  }, 100);
+  }, INTERVAL_TIME);
 
   register.registerMetric(g);
 };
