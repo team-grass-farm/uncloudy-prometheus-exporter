c9be27fdb19261cabce6fa43736272ffb944194e
Jaycol Kim <jaycol.kim@gmail.com>
2022-07-30 15:16:25 +0900

    Revert "feat: Develop uncloudy prometheus exporter"

--- metric/constants/labelName.js
+++
@@ -1,7 +0,0 @@
-const POD_LABEL_NAMES = ['pod', 'namespace', 'instance'];
-const NODE_LABEL_NAMES = ['container', 'cluster', 'instance'];
-
-module.exports = {
-  POD_LABEL_NAMES,
-  NODE_LABEL_NAMES,
-};
